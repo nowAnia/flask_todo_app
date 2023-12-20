ff313243e38512d3590e15f9e5dac85354ac3b66
Anna Nowak <ann.kawulaa@gmail.com>
2023-12-20 16:55:45 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
+
+
+@app.route('/')
+def index():
+    todo_list = Todo.query.all()
+
+    return render_template('base.html', todo_list=todo_list)
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+
+        app.run(debug=True)
